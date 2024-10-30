4c65e8fcecc643d906ca3f8bbe051134a930bdd5
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-10-30 15:53:14 -0300

    Criadas interfaces

---
+++ src/main/java/org/example/DepartmentDao.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+}
