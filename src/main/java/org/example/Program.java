af567cf7a794b77145593dde5136e249c84c76f9
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-10-30 15:46:49 -0300

    Adicionada classe Seller

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -6,6 +6,7 @@ public class Program {
     public static void main(String[] args) {
 
     Department obj = new Department(1, "Books");
-        System.out.println(obj);
+    Seller seller = new Seller(21, "Bob", "bob@gmail.com", 3000.0, obj);
+        System.out.println(seller);
     }
 }
