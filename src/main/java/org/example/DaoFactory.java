f30640535dec6b5ea182d10d89c9b4f46a3f54a7
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-10-31 17:26:49 -0300

    DepartmentDaoJDBC e DaoFactory

--- src/main/java/org/example/DaoFactory.java
+++ src/main/java/org/example/DaoFactory.java
@@ -5,4 +5,8 @@ public class DaoFactory {
     public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(Db.getConnection());
     }
+
+    public static DepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(Db.getConnection());
+    }
 }
